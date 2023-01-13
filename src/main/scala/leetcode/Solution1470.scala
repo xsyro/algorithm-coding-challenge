053fcae115aadac3d2d7537118ef1b11254a81f2
jamiu-au <86314518+jamiu-au@users.noreply.github.com>
2023-01-13 05:12:09 -0800

    solved solution 1470 on leetcode

--- src/main/scala/leetcode/Solution1470.scala
+++ src/main/scala/leetcode/Solution1470.scala
@@ -1,11 +1,23 @@
 package leetcode
 
-object Solution1470 extends App{
+import scala.annotation.tailrec
+
+object Solution1470 extends App {
 
 
   def shuffle(nums: Array[Int], n: Int): Array[Int] = {
+    val (x, y) = nums.zipWithIndex.partition(_._2 < n)
 
-    Array()
+    @tailrec
+    def joinItems(left: Array[Int], right: Array[Int], result: List[Int]): List[Int] = {
+      if (left.isEmpty || right.isEmpty) result else {
+        joinItems(left.tail, right.tail, left.head :: right.head :: result)
+      }
+    }
+
+    joinItems(x.map(_._1).reverse, y.map(_._1).reverse, List()).toArray
   }
 
+
+  println(shuffle(Array(2, 5, 1, 3, 4, 7), 3).mkString(","))
 }
