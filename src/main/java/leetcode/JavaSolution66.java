75b52f04ab063af7c0648a0e3f10d5f52548f91a
Jamiu Igbalajobi <kreatifcode@gmail.com>
2023-08-01 15:32:23 +0100

    solved leetcode solution 66 using java

---
+++ src/main/java/leetcode/JavaSolution66.java
@@ -0,0 +1,34 @@
+package leetcode;
+
+
+import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+public class JavaSolution66 {
+
+    /**
+     * You are given a large integer represented as an integer array digits, where each digits[i] is the ith digit of the integer.
+     * The digits are ordered from most significant to least significant in left-to-right order.
+     * The large integer does not contain any leading 0's.
+     * <p>
+     * Increment the large integer by one and return the resulting array of digits.
+     *
+     * @param digits
+     * @return
+     */
+    public int[] plusOne(int[] digits) {
+        StringBuffer stringBuffer = new StringBuffer();
+        for (int i = 0; i < digits.length; i++) {
+            stringBuffer.append(digits[i]);
+        }
+        BigDecimal digitAdd = new BigDecimal(stringBuffer.toString()).add(new BigDecimal(1));
+        String[] splitDigits = digitAdd.toString().split("");
+        int[] newDigits = new int[splitDigits.length];
+        for (int i = 0; i < splitDigits.length; i++) {
+            newDigits[i] = Integer.parseInt(splitDigits[i]);
+        }
+        return newDigits;
+    }
+};
