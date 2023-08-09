57a610f3b1798e30e7ff0df8d6f68557acd15075
Jamiu Igbalajobi <kreatifcode@gmail.com>
2023-08-09 15:38:39 +0100

    solved solution268 in java

---
+++ src/main/java/leetcode/JavaSolution268.java
@@ -0,0 +1,16 @@
+package leetcode;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class JavaSolution268 {
+
+    public int missingNumber(int[] nums) {
+        List<Integer> numsToList = new ArrayList<>(nums.length);
+        List<Integer> newNumToList = new ArrayList<>(nums.length + 1);
+        for(int i = 0; i < nums.length; i++) numsToList.add(nums[i]);
+        for(int i = 0; i <= nums.length; i++) newNumToList.add(i);
+        return newNumToList.stream().dropWhile(numsToList::contains).findFirst().get();
+    }
+
+}
