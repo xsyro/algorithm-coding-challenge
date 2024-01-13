d5fba05c58d0cc034588cb9c99b9b3e51888fe07
Igbalajobi Jamiu <kreatifcode@gmail.com>
2024-01-13 08:07:04 +0100

    Solved Solution169 of LeetCode Top 150 Interview Questions

--- go-lang/leetcode/solution-169.go
+++ go-lang/leetcode/solution-169.go
@@ -1,1 +1,29 @@
 package leetcode
+
+import (
+	"sort"
+)
+
+// https://leetcode.com/problems/majority-element
+func majorityElement(nums []int) int {
+	itemCount := map[int]int{}
+	for i := range nums {
+		currentCount, isExist := itemCount[nums[i]]
+		if isExist {
+			itemCount[nums[i]] = currentCount + 1
+		} else {
+			itemCount[nums[i]] = 1
+		}
+	}
+
+	keys := make([]int, 0, len(itemCount))
+	for k := range itemCount {
+		keys = append(keys, k)
+	}
+
+	sort.SliceStable(keys, func(i, j int) bool {
+		return itemCount[keys[i]] > itemCount[keys[j]]
+	})
+
+	return keys[0]
+}
