ec2fcdec2cbc18edb9d7191432af7ada73bd882d
Igbalajobi Jamiu <kreatifcode@gmail.com>
2024-01-14 23:57:08 +0100

    Solved Solution-1 of LeetCode Top 150 Interview Questions using O(n) time complexity

--- go-lang/leetcode/solution-1.go
+++ go-lang/leetcode/solution-1.go
@@ -1,1 +1,19 @@
 package leetcode
+
+func twoSum(nums []int, target int) []int {
+	var resultArr = make([]int, 0, 2)
+	iterationMap := map[int]int{}
+	for i := range nums {
+		rNums := target - nums[i]
+		c, ok := iterationMap[rNums]
+		if !ok {
+			iterationMap[nums[i]] = i
+			rNums = 0
+		} else {
+			resultArr = append(resultArr, c, i)
+			return resultArr
+		}
+	}
+	return resultArr
+
+}
