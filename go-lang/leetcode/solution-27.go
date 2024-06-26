ca0f5e5718bc39ac6ed97d808c50229167d9bffa
Igbalajobi Jamiu <kreatifcode@gmail.com>
2024-06-26 20:22:27 +0100

    implement leetcode solution-27 with Go

--- go-lang/leetcode/solution-27.go
+++ go-lang/leetcode/solution-27.go
@@ -1,1 +1,16 @@
 package leetcode
+
+import "slices"
+
+// https://leetcode.com/problems/remove-element/
+func removeElement(nums []int, val int) int {
+	//for i := range nums {
+	//	if nums[i] == val {
+	//
+	//	}
+	//}
+	nums = slices.DeleteFunc(nums, func(i int) bool {
+		return val == i
+	})
+	return len(nums)
+}
