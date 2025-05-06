6107fee57b9e0d1daa04a83a756de6dc91ebcadc
Igbalajobi Jamiu <kreatifcode@gmail.com>
2025-05-06 10:47:19 +0100

    solved solutution 349 using golang

---
+++ go-lang/leetcode/solution-349.go
@@ -0,0 +1,23 @@
+package leetcode
+
+import "slices"
+
+func intersection(nums1 []int, nums2 []int) []int {
+	var numSet1, numSet2, intersection []int
+	for _, item := range nums1 {
+		if !slices.Contains(numSet1, item) {
+			numSet1 = append(numSet1, item)
+		}
+	}
+	for _, item := range nums2 {
+		if !slices.Contains(numSet2, item) {
+			numSet2 = append(numSet2, item)
+		}
+	}
+	for _, item := range numSet1 {
+		if slices.Contains(numSet2, item) {
+			intersection = append(intersection, item)
+		}
+	}
+	return intersection
+}
